44b5b285138da46e34e7f4475521bdcb29a42720
ryzikovnikita924-afk <ryzikovnikita924@gmail.com>
2026-02-14 21:12:34 +0300

    Lab1

---
+++ lab1/src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import com.example.model.Character;
+import com.example.parser.ArrayListCSVParser;
+import java.util.List;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayListCSVParser a = new ArrayListCSVParser("characters.csv");
+        List<Character> arrayListUsers = a.parseAll();
+        TreeMapServ n = new TreeMapServ();
+        TreeMap<String, List<String>> FinalTreeMap = n.addTree(arrayListUsers);
+        n.printSpeciesMap(FinalTreeMap);
+        n.writeCSV(FinalTreeMap, "lab1/src/main/resources/output.csv");
+
+    }
+}
