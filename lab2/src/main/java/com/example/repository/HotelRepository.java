0739c4f500e21a2c3e7cb0a3385935c6239e6525
ryzikovnikita924-afk <ryzikovnikita924@gmail.com>
2026-03-31 12:48:10 +0300

    Lab3 Убрал Jooby

--- lab2/src/main/java/com/example/repository/HotelRepository.java
+++ lab2/src/main/java/com/example/repository/HotelRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface IHotelRepository {
+public interface HotelRepository {
     Optional<Hotels> getById(UUID id);
     Optional<Hotels> findByName(String name);
     List<Hotels> getAll();
